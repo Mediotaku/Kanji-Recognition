08be766dd822654e9ce5681a5165c29e71b9116b
Mediotaku <Mediotaku@gmail.com>
2019-12-19 13:29:27 +0100

    Red convolucional mas compleja Hiragana
    Se han añadido capas de complejidad a la red convolucional que usa Hiragana. Ahora mismo no guarda los resultados.

--- hiraganaCNN.py
+++ hiraganaCNN.py
@@ -30,11 +30,14 @@ num_classes = y_test.shape[1]
 def baseline_model():
 	# create model
 	model = Sequential()
-	model.add(Conv2D(32, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+	model.add(Conv2D(30, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Conv2D(15, (3, 3), activation='relu'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(0.2))
 	model.add(Flatten())
 	model.add(Dense(128, activation='relu'))
+	model.add(Dense(50, activation='relu'))
 	model.add(Dense(num_classes, activation='softmax'))
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
