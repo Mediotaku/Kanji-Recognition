db58b1925f961edf1d5ecfa11208ff88aa40b7f3
Mediotaku <Mediotaku@gmail.com>
2020-02-20 13:40:36 +0100

    Añadido bucle de lectura en clase get_ETL8B2
    Ahora se leen los caracteres de cada escritor de cada clase dentro de una nueva función get_ETL8B2.

--- ETLtest.py
+++ ETLtest.py
@@ -1,5 +1,7 @@
 import struct
 from PIL import Image
+#This test is working on Python 2.7.17
+
 #Here we read the database as indicated by the ETL
 def read_record_ETL8B2(f):
     s = f.read(512)
@@ -8,19 +10,27 @@ def read_record_ETL8B2(f):
     return r + (i1,)
 
 
+#Here we get the data using read_record with the correct parameters
+#categories-> classes
+#writers-> examples per class
+def get_ETL8B2(categories, writers):
+    filename = 'ETL8B2C1'
+    #Creating one kanji image, then we will create a dataset for keras with them
+    new_img = Image.new('1', (64, 64))
 
-filename = 'ETL8B2C1'
-id_category = -1
-new_img = Image.new('1', (64*16, 64*10))
+    iter(categories)
 
-with open(filename, 'r') as f:
-    
-    for i in range(160):
-        id_category=id_category+1
-        f.seek((id_category * 160 + 1) * 512)
-        r = read_record_ETL8B2(f)
-        new_img.paste(r[-1], (64*(i%16), 64*(i/16)))
+    for id_category in categories:
+        with open(filename, 'r') as f:
+            f.seek((id_category * 160 + 1) * 512) #In case of ETL8B2C1 dataset
+            for i in range(writers):
+                r = read_record_ETL8B2(f)
+                new_img.paste(r[-1], (0,0))
+                #Image.eval applies a function to each pixel of the given image
+                iI = Image.eval(new_img, lambda x: not x)
+                #Formating id_category and i into string to create 
+                #a filename for every writer from each class (avoiding overwrite)
+                fn = 'ETL8B2_{:s}.png'.format(str(id_category)+str(i))
+                iI.save(fn, 'PNG')
 
-iI = Image.eval(new_img, lambda x: not x)
-fn = 'ETL8B2_{:03d}.png'.format(id_category)
-iI.save(fn, 'PNG')
+get_ETL8B2(range(0,12),5)
