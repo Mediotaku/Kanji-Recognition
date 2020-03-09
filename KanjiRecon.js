33dc79c54c245ba658f89bf081379206c6feb284
Mediotaku <Mediotaku@gmail.com>
2020-03-09 13:57:28 +0100

    Mejorado dibujado, añadido soporte táctil, grid lines y boton clean
    Se ha mejorado el dibujado del canvas y añadido soporte para touch events tras investigar sobre las formas de encontrar la posición del ratón, además se ha añadido el grid de caligrafia para kanjis y un botón de limpiar lo escrito.

--- KanjiRecon.js
+++ KanjiRecon.js
@@ -9,20 +9,81 @@ window.onload = function(){
 
 function iniciar(canvas, ctx){
     var click=false;
-     canvas.onmousedown=function(e){
-        click=true;
-     }
-     canvas.onmouseup=function(e){
-        click=false;
+    function grid(){
+        //Drawing 原稿用紙 grid lines in lightgrey
+        ctx.strokeStyle="lightgrey";
+        ctx.lineWidth = 1;
+        ctx.lineCap= "butt";
         ctx.beginPath();
-     }
-     canvas.onmousemove=function(e){
-         if(click){
-             console.log(e.offsetX, e.offsetY);
-             ctx.lineWidth=3;
-             ctx.lineCap= "round";
-             ctx.lineTo(e.offsetX, e.offsetY);
-             ctx.stroke();
-         }
-     }
+        ctx.setLineDash([10,10]);
+        //+5 to make lines symmetrical
+        ctx.moveTo(0+5,canvas.width/2);
+        ctx.lineTo(canvas.height,canvas.width/2);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.setLineDash([10,10]);
+        ctx.moveTo(canvas.width/2,0+5);
+        ctx.lineTo(canvas.width/2,canvas.height);
+        ctx.stroke();
+        //Return to standard writing settings
+        ctx.setLineDash([]);
+        ctx.strokeStyle="black";
+    }
+
+    grid();
+    
+    canvas.onmousedown=function(e){
+       click=true;
+       ctx.beginPath();
+    }
+    canvas.addEventListener("touchstart", function (e){
+       click=true;
+       ctx.beginPath();
+       e.preventDefault();
+    }); 
+    canvas.onmouseup=function(e){
+       click=false;
+       ctx.beginPath();
+    }
+    canvas.addEventListener("touchend", function (e){
+       click=false;
+       ctx.beginPath();
+       e.preventDefault();
+    }); 
+    canvas.onmousemove=function(e){
+       if(click){
+           //getBoundingClientRect() returns the position of an element relative to the viewport
+           //this function don't take into account an style applied to the canvas like border or padding 
+           //Get the clientX/Y and subtract the canvas top left to get the correct position
+           var rect = canvas.getBoundingClientRect();
+           var clickX = e.clientX - rect.left;
+           var clickY = e.clientY - rect.top;
+           ctx.lineWidth = 3;
+           ctx.lineCap= "round";
+           ctx.lineTo(clickX, clickY);
+           ctx.stroke();
+       }
+    }
+    canvas.addEventListener("touchmove", function (e){
+       if(click){
+           var touch = e.touches[0];
+           //pageX/Y returns the position relative to the top left corner of the whole page, scroll included
+           //clientX/Y returns the position relative to the top left corner of the visible page in that moment
+           //When using either clientX/Y or pageX/Y, we have to subtract the offset of the canvas to get
+           //the correct position relative to its top left corner.  
+           var touchX = touch.pageX - touch.target.offsetLeft;
+           var touchY = touch.pageY - touch.target.offsetTop;
+           ctx.lineWidth = 3;
+           ctx.lineCap= "round";
+           ctx.lineTo(touchX, touchY);
+           ctx.stroke();
+       }
+       e.preventDefault();
+    }); 
+    document.getElementById("clean").onclick=function(){
+       //The enternal battle against width hack 
+       ctx.clearRect(0, 0, canvas.width, canvas.height);
+       //Draw grid lines after clear
+       grid();
+    }
 }
